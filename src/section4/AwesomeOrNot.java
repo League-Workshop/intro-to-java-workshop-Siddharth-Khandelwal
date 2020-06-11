282d1c1b7af9b54a877ca81f85601bd408161e3d
root <root@e113f84bd91c>
2020-06-11 21:56:12 +0000

    Day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,31 +2,49 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
+	
+	
+	
+	Random rand = new Random();
+	int random = rand.nextInt(4);
 	// 3. Print out this variable
-
+	JOptionPane.showMessageDialog(null, random);
 	// 4. Get the user to enter something that they think is awesome
-
+String awesome = JOptionPane.showInputDialog("What is something awesome?");
 	// 5. If the random number is 0
-
+if(random == 0) {
+	JOptionPane.showMessageDialog(null,awesome+ " is awesome" );
+}
 	// -- tell the user whatever they entered is awesome!
 
 	// 6. If the random number is 1
-
+if (random == 1) {
+	JOptionPane.showMessageDialog(null,awesome+" is ok " );
+}
 	// -- tell the user whatever they entered is ok.
 
 	// 7. If the random number is 2
-
+if(random == 2) {
+	JOptionPane.showMessageDialog(null, awesome+" is boring");
+}
 	// -- tell the user whatever they entered is boring.
 
 	// 8. If the random number is 3
 
 	// -- write your own answer
+}
+
+
 
 }
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -9,14 +9,19 @@ public class QuizGame {
 		// 1.  Create a variable to hold the user's score 
 		int score = 0;
 		// 2.  Ask the user a question 
-		JOptionPane.showInputDialog("What is 3+3-6=2? ");
+		String input = JOptionPane.showInputDialog("What is 3+3-6+2? ");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (input.equals("2")) {
+			score+=1;
+		}
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String input2 = JOptionPane.showInputDialog("What is 7+2-6+3x4?");
+		if (input2.contentEquals("15")) {
+			score+=1;
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null, "You got " +score+" out of 2 correct");
 	}
 }
